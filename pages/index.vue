56f07cfd4120a34b06e8480389a4f25d3977a3ab
MEHDI BELLAM <porcepic91@gmail.com>
2024-03-21 15:58:48 +0100

    fin EX filtre

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,41 @@
 
 
 const page = ref(1)
-const Pagesize = ref(2)
+const Pagesize = ref(10)
 const filters = ref([])
+let filteredData = ref([])
 
+function addToFilters(param) {
+  if (!filters.value.includes(param)) {
+    filters.value.push(param)
+    console.log("FILTER VALUE" ,filters.value)
+    console.log(data)
+  } else {
+    filters.value = filters.value.filter(e => e !== param)
+    console.log("FILTER VALUE" ,filters.value)
+    console.log(data)
+  }
+}
+
+
+  
 const { find } = useStrapi()
+// Nom de la collection
 // Nom de la collection
 const { data, pending: bosesPending, error } = useAsyncData('boses', () => {
   return find('boses', {
     populate: '*',
+    
   })
-
+}, {
+  watch: [page, Pagesize, filters]
 })
+
+
+
+
+
+
 
 const { data: location, pending: locationPending  , refresh} = useAsyncData('locations', () => {
   return find('locations', {
@@ -21,51 +45,72 @@ const { data: location, pending: locationPending  , refresh} = useAsyncData('loc
       page: page.value,
       pageSize: Pagesize.value
     },
-    filters: {
-      location: {
-        $in: filters.value
-      }
-    }
-
-  }), {
-    watch: [page, Pagesize, filters],
-  }
     
 
+  })
+    
+
+},{
+    watch: [page, Pagesize, filters],
 })
 
-refresh()
 
+
+console.log(location)
 
 </script>
 <template>
   <div>
     <h1>Bienvenue</h1>
-   
+
     <template v-if="bosesPending">
-        <span>Loading...</span>
+      <span>Loading...</span>
     </template>
     <template v-else>
-      
+      {{ filteredData }}
+
       <h2>Location</h2>
       <div class="LocattionList">
         <div v-for="e in location?.data" class="" :key="e.slug">
-          <button>{{ e.location }}</button>
-          
+          <button @click="addToFilters(e.location)">{{ e.location }}</button>
+
+          <a> {{ location.data.location }}</a>
+
         </div>
 
       </div>
-      <pre> {{ location }}</pre>
+      <div> {{ filters }}</div>
+
+      <h1> BOSS filtré</h1>
+
+      <div v-for="e in data?.data">
+        <div v-if="filters.includes(e.Bosse_location.location) ">
+
+          <a>{{ e.name }} {{ e.Surname }}</a>
+          <a> {{ e.image.formats.small }}</a>
+
+          <img
+            :src="e.image.formats.small.url || `https://image.api.playstation.com/vulcan/ap/rnd/202107/1612/Y5RHNmzAtc6sRYwZlYiKHAxN.png`"  alt="
+            image" />
+
+        </div>
+      </div>
+
+
+
+
+
 
 
 
       <h2>Les boses</h2>
       <div v-for="e in data?.data" :key="e.slug">
-        <a :href="`/boses/${e.slug}`"  :key="e.id"  class="underline">{{ e.name }}</a>
+        <a :href="`/boses/${e.slug}`" :key="e.id" class="underline">{{ e.name }}</a>
       </div>
-     
+
+
     </template>
-    
+
   </div>
 </template>
 
@@ -85,7 +130,7 @@ button {
   
   border: 1px solid #000;
   border-radius: 5px;
-  color: #000;
+  /* color: #000; */
   display: flex;
   flex-direction: row;
   width: auto;
